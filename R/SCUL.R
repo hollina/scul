4787033fb4d1ee3c85336480632cf3daeacde286
Alex Hollingsworth <hollinal@indiana.edu>
2021-05-16 13:43:03 -0400

    add ability to use different cross-validated lambda

--- R/SCUL.R
+++ R/SCUL.R
@@ -23,6 +23,11 @@
 #' @param  TreatmentBeginsAt An integer indicating which row begins treatment.
 #'         Default is to begin this at SCUL.input$TreatmentBeginsAt.
 #' @param TrainingPostPeriodLength The number of timer periods post-treatment for training data. Defaults to all time since treatment begins.
+#' @param cvOption Do you want to use the median CV lambda (cvOption = lambda.median),
+#'         one that produces the minimum MSE (cvOption = lambda.min), or the largest
+#'         lambda that produces a MSE within one standard error of the minimum MSE
+#'         (cvOption = lambda.1se).
+#'         Default is lambda.1se
 #'
 #' @return OutputDataSCUL  A list with the synthetic series, weights used to construct the series, and measures of fit.
 #' @import glmnet
@@ -39,102 +44,167 @@ SCUL <- function(
                   time = SCUL.input$time,
                   y.actual = SCUL.input$y,
                   TreatmentBeginsAt = SCUL.input$TreatmentBeginsAt,
-                  TrainingPostPeriodLength = SCUL.input$TrainingPostPeriodLength
-)
-{
+                  TrainingPostPeriodLength = SCUL.input$TrainingPostPeriodLength,
+                  cvOption = lambda.1se
+){
+  ### Function to standardize variables: (need to use n instead of (n-1) as denominator)
+  ### Note: This came from https://stackoverflow.com/questions/23686067/default-lambda-sequence-in-glmnet-for-cross-validation
+  mysd <- function(z) sqrt(sum((z-mean(z))^2)/length(z))
+
+  # Function to get optimal cv. Simple adaptation from getOptcv.glmnet
+  # Function to get optimal cv
+  getOptcv.scul <-
+    function (lambdapath, mse, se, fullMSE)
+    {
+      # What is the minimum MSE
+      cvmin = min(mse, na.rm = TRUE)
+
+      # Where is the minimum MSE
+      idmin = mse <= cvmin
+
+      lambda.min = max(lambdapath[idmin], na.rm = TRUE)
+      idmin = match(lambda.min, lambdapath)
+      semin = (mse + se)[idmin]
+
+      id1se = mse <= semin
+      lambda.1se = max(lambdapath[id1se], na.rm = TRUE)
+      id1se = match(lambda.1se, lambdapath)
+
+      # Find the column id of the minimum cv in each row. then take the median
+      idmedian <-  median(apply(fullMSE, 1, which.min))
+
+      # Find the lambda associated with this median.
+      lambda.median = max(lambdapath[idmedian], na.rm = TRUE)
+      idmedian = match(lambda.median, lambdapath)
+
+      index=matrix(c(idmin,id1se, idmedian),3,1,dimnames=list(c("min", "1se", "median"),"Lambda"))
+      list(lambda.min = lambda.min, lambda.1se = lambda.1se,lambda.median = lambda.median, index = index)
+    }
 
   # Determine the maximum number of rolling-origin k-fold cross validations that can occur
   MaxCrossValidations <- PrePeriodLength-NumberInitialTimePeriods-TrainingPostPeriodLength+1
   # Determine the last position of the largest possible training dataset that only uses pre-treatment data
   StoppingPoint <- NumberInitialTimePeriods+MaxCrossValidations-1
 
-  # Initialize a matrix of 0's, that is 1 by max # of C.V. runs
-  MinimumLambdaRolling<-matrix(0,1,MaxCrossValidations)
+  # What are the number of lambdas we want to search over.
+  MaxLambdasInGrid <- 100 # by default glmnet won't go passed 100, but it may stop earlier. So picking 100 here ensures that this list is contained.
 
-  # Preform the task once for the correct number of times
-  for (i in NumberInitialTimePeriods:StoppingPoint){
-    #i <- NumberInitialTimePeriods
-    j = i -NumberInitialTimePeriods + 1
+    ## Calculate the max-lambda across all of the cross-validation runs.
+    MaxLambdaList <- matrix(0, 1, MaxCrossValidations)
 
-    # Specify training data
-    x.training <-x.DonorPool.PreTreatment[1:i,] # this had me switch row and column. Why?
-    y.training <-y.PreTreatment[1:i] # this had me switch row and column. Why?
+      ### First calculate the max lambda for each cross-validation run.
+      for (i in NumberInitialTimePeriods:StoppingPoint){
+        #i <- NumberInitialTimePeriods
+        j = i - NumberInitialTimePeriods + 1
 
-    # Run a lasso with only the training data. It will run on for a grid of lambdas by default
-    fit=glmnet(x.training,y.training)
+        # Specify training data
+        x.training <-x.DonorPool.PreTreatment[1:i,] # this had me switch row and column. Why?
+        y.training <-y.PreTreatment[1:i] # this had me switch row and column. Why?
 
-    # Extract the entire grid of lambdas from the training data
-    LambdaList = fit$lambda
+        sx <- scale(x.training, scale = apply(x.training, 2, mysd))
+        sx <- as.matrix(x.training, ncol = 20, nrow = 100)
 
-    # Determine when the testing data begins for this CV run
-    BeginingOfTestData = i +1
+        # Get lambda_max: See https://stackoverflow.com/questions/25257780/how-does-glmnet-compute-the-maximal-lambda-value/
+        MaxLambdaList[j] <- max(abs(colSums(sx*y.training)))/nrow(sx)
+      }
 
-    # Determine when the testing data ends for this CV run
-    EndOfTestData = i + TrainingPostPeriodLength
+      ### Now calculate the lambda sequence
+      epsilon <- .0001 # default used in glmnet
 
-    # Specify testing data
-    x.testing <-x.DonorPool.PreTreatment[BeginingOfTestData:EndOfTestData,]
-    y.testing <-y.PreTreatment[BeginingOfTestData:EndOfTestData,]
+      lambdapath <- round(exp(seq(log(max(MaxLambdaList)), log(max(MaxLambdaList)*epsilon),
+                                  length.out = MaxLambdasInGrid)), digits = 10)
+      # Initialize a matrix of 0's, that is # of lambdas in grid by max # of C.V. runs
+      # MinimumLambdaRolling <- matrix(0, MaxLambdasInGrid, MaxCrossValidations)
 
-    # Create a predicted y value for the entire pre-treatment time period for each lambda in the the exact grid of lambdas from the training data
-    prediction<-predict(fit,newx=x.DonorPool.PreTreatment,x=x.training, y=y.training, s=fit$lambda,exact=TRUE)
+      # Initialize a matrix of 0's, that is # of lambdas in grid by max # of C.V. runs
+      MinimumLambdaRollingMSE <- matrix(0, MaxLambdasInGrid, MaxCrossValidations)
 
+      # Preform lasso for each cross-validation run and store test MSE for each run for each lambda in grid
+      for (i in NumberInitialTimePeriods:StoppingPoint){
+        #i <- NumberInitialTimePeriods
+        j = i -NumberInitialTimePeriods + 1
 
-    # Find the lambda that results in the minimum mean squarred error
+        # Specify training data
+        x.training <-x.DonorPool.PreTreatment[1:i,] # this had me switch row and column. Why?
+        y.training <-y.PreTreatment[1:i] # this had me switch row and column. Why?
 
-    # first take the difference from the prediction and the test data
-    mse <- prediction[BeginingOfTestData:EndOfTestData,]  -y.testing
+        # Run a lasso with only the training data. It will run on for a grid of lambdas by default
+        fit = glmnet(x.training, y.training, lambda = lambdapath)
 
-    # Then square each term
-    for (z in 1:length(mse)) {
-      mse[z] <- mse[z]^2
-    }
+        # Determine when the testing data begins for this CV run
+        BeginingOfTestData = i +1
 
-    # Now findind the minimum squarred error, how we do this depends on the length of the post-period
-    # Due to how glmnet stores the optimal lambda, we need an if statement for different post-period lengths. If the post-period is only one time period, it will be stored as a single number and if it's longer it will be a vector. The if statement makes sure it is stores as the same type no matter what
-    if (PostPeriodLength < 2) {
-      # Create a vector, that dat (a sinlge column)
-      newV <- as.vector(mse[,1])
-      # extract the lamda that minimizes the prediction error
-      MinimumLambdaRolling[j]<- LambdaList[which.min(newV)]
-    }
+        # Determine when the testing data ends for this CV run
+        EndOfTestData = i + TrainingPostPeriodLength
 
-    if (PostPeriodLength > 1) {
-      # extract the lamda that minimizes the prediction error summed across each time period in the test data
-      MinimumLambdaRolling[j]<-LambdaList[which.min(colSums(mse))]
-    }
+        # Specify testing data
+        x.testing <-x.DonorPool.PreTreatment[BeginingOfTestData:EndOfTestData,]
+        y.testing <-y.PreTreatment[BeginingOfTestData:EndOfTestData,]
 
-    #Display the output. Useful for debugging.
-    print("The training data is running from 1 to")
-    print(i)
-    print("The testing data is running from")
-    print(BeginingOfTestData)
-    print("to")
-    print(EndOfTestData)
-    print("The minimum lambda is")
-    print(MinimumLambdaRolling[j])
-  }
+        # Create a predicted y value for the entire pre-treatment time period for each lambda in the the exact grid of lambdas from the training data
+        prediction <- predict(fit,
+                              newx = x.DonorPool.PreTreatment,
+                              x = x.training,
+                              y = y.training,
+                              s = lambdapath,
+                              exact = TRUE)
+        # Squared error
+        squared_error <- (prediction[BeginingOfTestData:EndOfTestData,] - y.testing)^2
 
-  # Calculate cross-validated lambda
-  CrossValidatedLambda<-median(MinimumLambdaRolling)
+        # Mean squared error
+        mse <- colMeans(squared_error)
 
-  # Run the actual fit
-  EntirePretreatmentFit=glmnet(x.DonorPool.PreTreatment,y.PreTreatment)
+        # Save MSE
+        MinimumLambdaRollingMSE[j, ] <- mse
 
-  # Extract the exact prediction for the cross-validated lambda
-  y.scul <- predict(x=x.DonorPool.PreTreatment, y=y.PreTreatment, newx = as.matrix(x.DonorPool), EntirePretreatmentFit,s = CrossValidatedLambda, exact = TRUE)
+      }
 
-  # Extract the exact coefficients used
-  coef.exact = coef(x=x.DonorPool.PreTreatment,y=y.PreTreatment,EntirePretreatmentFit, s = CrossValidatedLambda, exact = TRUE)
+        # Take mean of MSE to get mean MSE for each lambda across all CV runs
+        cvMSE <- colMeans(MinimumLambdaRollingMSE)
 
-  # Calculate the standard deviation of the outcome variable in the pre-treatment period
-  PreTreatmentSD <-sd(y.PreTreatment[1:TreatmentBeginsAt-1])
+        # Calculate standard error of MSE
+        # length of y.testing*number of runs is N for SE calculation.
+        cvSE <- sqrt(cvMSE/(TrainingPostPeriodLength*MaxCrossValidations-1))
 
-  # Take the absolute value of the difference between the predicition and the actual data divided by the standard deviation
-  CohensD <- abs(y.scul[1:(TreatmentBeginsAt-1),]-y.actual[1:(TreatmentBeginsAt-1),])/PreTreatmentSD
+        # Extract 1) lambda that minimizes cvMSE, 2) max lambda that prodcuses an MSE within one standard error of the minimum 3) and median min lambda.
+        lambda <- getOptcv.scul(lambdapath = lambdapath,
+                      mse = cvMSE,
+                      se = cvSE,
+                      fullMSE = MinimumLambdaRollingMSE)
 
-  # Calculate the mean pre-treatment cohen's D
-  MeanCohensD <- mean(CohensD)
+      # Based on options, pick cross validated lambda
+       CrossValidatedLambda <- switch(cvOption,
+                                      lambda.1se = lambda$lambda.1se,
+                                      lambda.min = lambda$lambda.min,
+                                      lambda.median = lambda$lambda.median,
+       )
+
+      # Run the actual fit
+      EntirePretreatmentFit = glmnet(x = x.DonorPool.PreTreatment,
+                                     y = y.PreTreatment)
+
+      # Extract the exact prediction for the cross-validated lambda
+      y.scul <- predict(x = x.DonorPool.PreTreatment,
+                        y = y.PreTreatment,
+                        newx = as.matrix(x.DonorPool),
+                        EntirePretreatmentFit,
+                        s = CrossValidatedLambda,
+                        exact = TRUE)
+
+      # Extract the exact coefficients used
+      coef.exact = coef(x = x.DonorPool.PreTreatment,
+                        y = y.PreTreatment,EntirePretreatmentFit,
+                        s = CrossValidatedLambda,
+                        exact = TRUE)
+
+      # Calculate the standard deviation of the outcome variable in the pre-treatment period
+      PreTreatmentSD <- sd(y.PreTreatment[1:TreatmentBeginsAt-1])
+
+      # Take the absolute value of the difference between the predicition and the actual data divided by the standard deviation
+      CohensD <- abs(y.scul[1:(TreatmentBeginsAt-1),]-y.actual[1:(TreatmentBeginsAt-1),])/PreTreatmentSD
+
+      # Calculate the mean pre-treatment cohen's D
+      MeanCohensD <- mean(CohensD)
 
   # Wrap all of the items we will need for later into a list.
   OutputDataSCUL <- list(
@@ -144,7 +214,7 @@ SCUL <- function(
     CrossValidatedLambda = CrossValidatedLambda,
     TreatmentBeginsAt = TreatmentBeginsAt,
     CohensD = MeanCohensD,
-    coef.exact =coef.exact
+    coef.exact = coef.exact
   )
   # Return list
   return(OutputDataSCUL)
---
+++ R/to-delete/scratch.R
@@ -0,0 +1,118 @@
+rm(list=ls())
+## Load library and generate some data to illustrate:
+library("glmnet")
+set.seed(1234)
+n <- 100
+x1 <- matrix(rnorm(n*3), n, 3)
+y1 <- rnorm(n) + x1[ ,1]*3 + x1[ ,2]*-1 + x1[ , 3]*.5
+
+x1a <- x1[1:50 , ]
+x1b <- x1[51:100 , ]
+
+x2 <- matrix(rnorm(n*3), n, 3)
+y2 <- rnorm(n) + x2[ ,1]*3 + x2[ ,2]*-1 + x2[ , 3]*.5
+
+## Standardize variables: (need to use n instead of (n-1) as denominator)
+mysd <- function(z) sqrt(sum((z-mean(z))^2)/length(z))
+sx1 <- scale(x1, scale = apply(x1, 2, mysd))
+sx1 <- as.matrix(sx1, ncol = 20, nrow = 100)
+
+sx2 <- scale(x2, scale = apply(x2, 2, mysd))
+sx2 <- as.matrix(sx2, ncol = 20, nrow = 100)
+
+
+## Calculate lambda path (first get lambda_max):
+lambda_max <- max(abs(colSums(sx1*y1)))/n
+epsilon <- .0001
+K <- 45
+
+lambdapath <- round(exp(seq(log(lambda_max), log(lambda_max*epsilon),
+                            length.out = K)), digits = 10)
+
+## Run GLMnet
+fitGLM1 <- glmnet(sx1[1:50,], y1[1:50], lambda = lambdapath)
+fitGLM2 <- glmnet(sx1[1:60,], y1[1:60], lambda = lambdapath)
+fitGLM3 <- glmnet(sx1[1:70,], y1[1:70], lambda = lambdapath)
+
+
+prediction1 <- predict(fitGLM1,
+                      newx = sx1[71:80,] ,
+                      x = sx1[1:50,],
+                      y =  y1[1:50],
+                      s = lambdapath,
+                      exact = TRUE)
+
+prediction2 <- predict(fitGLM2,
+                       newx = sx1[71:80,] ,
+                       x = sx1[1:60,],
+                       y =  y1[1:60],
+                       s = lambdapath,
+                       exact = TRUE)
+prediction3 <- predict(fitGLM3,
+                       newx = sx1[71:80,] ,
+                       x = sx1[1:70,],
+                       y =  y1[1:70],
+                       s = lambdapath,
+                       exact = TRUE)
+# Squared error
+squared_error1 <- (prediction1 - y1[61:70])^2
+squared_error2 <- (prediction2 - y1[61:70])^2
+squared_error3 <- (prediction3 - y1[61:70])^2
+
+# Mean squared error
+mse1 <- colMeans(squared_error1)
+mse2 <- colMeans(squared_error2)
+mse3 <- colMeans(squared_error3)
+
+MinimumLambdaRollingMSE <- rbind(mse1, mse2, mse3)
+
+# Joint MSE
+# Take mean of MSE to get mean MSE for each lambda across all CV runs
+cvMSE <- colMeans(MinimumLambdaRollingMSE)
+
+# Calculate standard error of MSE
+# length of y.testing*number of runs is N for SE calculation.
+cvSE <- sqrt(cvMSE/(20-1))
+
+# Function to get optimal cv
+getOptcv.scul <-
+  function (lambdapath, mse, se, fullMSE)
+  {
+    # What is the minimum MSE
+    cvmin = min(mse, na.rm = TRUE)
+
+    # Where is the minimum MSE
+    idmin = mse <= cvmin
+
+    lambda.min = max(lambdapath[idmin], na.rm = TRUE)
+    idmin = match(lambda.min, lambdapath)
+    semin = (mse + se)[idmin]
+
+    id1se = mse <= semin
+    lambda.1se = max(lambdapath[id1se], na.rm = TRUE)
+    id1se = match(lambda.1se, lambdapath)
+
+    # Find the column id of the minimum cv in each row. then take the median
+    idmedian <-  median(apply(fullMSE, 1, which.min))
+
+    # Find the lambda associated with this median.
+    lambda.median = max(lambdapath[idmedian], na.rm = TRUE)
+    idmedian = match(lambda.median, lambdapath)
+
+    index=matrix(c(idmin,id1se, idmedian),3,1,dimnames=list(c("min", "1se", "median"),"Lambda"))
+    list(lambda.min = lambda.min, lambda.1se = lambda.1se,lambda.median = lambda.median, index = index)
+  }
+
+# Extract 1) lambda that minimizes cvMSE, 2) max lambda that prodcuses an MSE within one standard error of the minimum 3) and median min lambda.
+lambda <- getOptcv.scul(lambdapath = lambdapath,
+              mse = cvMSE,
+              se = cvSE,
+              fullMSE = MinimumLambdaRollingMSE)
+
+cvOption <- "lambda.1se"
+CrossValidatedLambda <- switch(cvOption,
+                               lambda.1se = lambda$lambda.1se,
+                               lambda.min = lambda$lambda.min,
+                               lambda.median = lambda$lambda.median,
+)
+CrossValidatedLambda
